e9c66c7bf089c36d13d9cef80b8fc54cbeef8f34
matgr1 <matgr1@gmail.com>
2017-10-06 12:50:23 -0400

    JGraph workaround

--- neatsample/src/main/java/matgr/ai/neatsample/App.java
+++ neatsample/src/main/java/matgr/ai/neatsample/App.java
@@ -9,6 +9,12 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     public static void main(String[] args) {
+
+        // TODO: without this, JGraphT eventually blows up... find a better way, this is terrible... (also, this may
+        //       need to be passed on the commandline if something causes Arrays to be initialized before this is
+        //       called... look for errors coming from JGraph about comparisons violating contracts...)
+        System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
+
         launch(args);
     }
 
